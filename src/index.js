113f48f045156e3bf52ea5b7cb1750ce7d81d582
twinklelixn <xiaonanli@163.com>
2020-08-18 21:23:16 +0800

    useSate 1.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let _state
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function myUseState(initialValue){
+  if(_state === undefined){
+    _state = initialValue
+  }
+  const setState = (newValue)=>{
+    _state = newValue
+    render()
+  }
+  return [_state, setState]
+}
+
+function render(){
+  ReactDOM.render(<App/>,document.getElementById('root'));
+}
+
+function App(){
+  const [n, setN] = myUseState(0)
+  return (
+    <div>
+      n: {n}
+      <button onClick={() => setN(n+1)}>+1</button>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
