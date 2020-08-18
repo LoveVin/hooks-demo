9b1b120616d977e282774f4f241701b9e49988b8
twinklelixn <xiaonanli@163.com>
2020-08-18 22:06:36 +0800

    useState 2.0

--- src/index.js
+++ src/index.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let _state
+let _state = []
+let index = 0
 
 function myUseState(initialValue){
-  if(_state === undefined){
-    _state = initialValue
+  const currentIndex = index
+  if(_state[currentIndex] === undefined){
+    _state[currentIndex] = initialValue
   }
   const setState = (newValue)=>{
-    _state = newValue
+    _state[currentIndex] = newValue
     render()
   }
-  return [_state, setState]
+  index++
+  return [_state[currentIndex], setState]
 }
 
 function render(){
+  index = 0
   ReactDOM.render(<App/>,document.getElementById('root'));
 }
 
 function App(){
   const [n, setN] = myUseState(0)
+  const [m, setM] = myUseState(0)
   return (
     <div>
       n: {n}
       <button onClick={() => setN(n+1)}>+1</button>
+      <br/>
+      m: {m}
+      <button onClick={() => setM(m+1)}>+1</button>
     </div>
   )
 }
