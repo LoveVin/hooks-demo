d231e6478ce1a83b09c628eb7db8965a28bdd17b
twinklelixn <xiaonanli@163.com>
2020-08-20 22:14:54 +0800

    useImperativeHandle

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import React, { useEffect , useState, useMemo} from 'react';
+import React, { useEffect , forwardRef, useRef, useImperativeHandle} from 'react';
 import ReactDOM from 'react-dom';
 
 function App(){
-  const [n, setN] = useState(0)
-  const [m, setM] = useState(0)
-  const onClickChild = useMemo(()=>{
-    return () => {
-      console.log(m)
-    }
-  },[m])  
+  const myRef = useRef(null)
+  useEffect(()=>{
+    console.log(myRef.current.real)
+    console.log(myRef.current.getParent())
+  }, [])
   return (
     <div>
       我是父组件
-      n: {n}
-      <button onClick={()=>setN(n+1)}>n+1</button>
-      <button onClick={()=>setM(m+1)}>m+1</button>
-      <Child value={m} onClick = {onClickChild}/>
+      <Child ref={myRef}/>
     </div>
   )
 }
 
-const Child = React.memo((props)=>{
-  useEffect(()=>{
-    console.log('子组件 render 了')
+const Child = forwardRef((props, ref)=>{
+  const childRef = useRef(null)
+  useImperativeHandle(ref, ()=>{
+    return {
+      real: childRef.current,
+      getParent(){
+        return childRef.current.parentNode
+      }
+    }
   })
   return (
     <div>
-      我是子组件，我收到来自父组件的值为：m {props.value}
-      <br/>
-      <button onClick={props.onClick}>click</button>
+      我是子组件，我有一个子DOM
+      <button ref={childRef}>按钮</button>
     </div>
   )
 })
